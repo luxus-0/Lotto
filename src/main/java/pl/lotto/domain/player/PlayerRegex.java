c9ac51cbb0732a7e69cb8fcb73b142ca4bc66328
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-14 19:26:42 +0200

    Remove unused class

--- src/main/java/pl/lotto/domain/player/PlayerRegex.java
+++
@@ -1,6 +0,0 @@
-package pl.lotto.domain.player;
-
-class PlayerRegex {
-    static String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#!^&*])[A-Za-z\\d@#!^&*]{8,}$";
-    static String PASSWORD_MESSAGE_REGEX = "Password must contain at least one uppercase letter, one digit, one special character (@#!^&*), and be at least 8 characters long.";
-}

fa238cb91eac39e11361e891db37054474bd6fa0
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-16 23:25:38 +0200

    Revert "feat(player):"
    This reverts commit 5d358ed00861b2c467b0086edcec1081dcd255b6.

--- src/main/java/pl/lotto/domain/player/PlayerService.java
+++ src/main/java/pl/lotto/domain/player/PlayerService.java
@@ -1,75 +1,47 @@
 package pl.lotto.domain.player;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import pl.lotto.domain.player.dto.*;
-import pl.lotto.infrastructure.security.JwtService;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
-/*
-*Dokończyć metodę: String generateToken() z JWT
-* Dokończyć metodę: PlayerStatisticsDto getPlayerStatistics(UUID playerId);
-* */
 @Service
 @Log4j2
-@AllArgsConstructor
 class PlayerService {
     private final PlayerRepository playerRepository;
     private final PlayerProfileRepository playerProfileRepository;
     private final PasswordEncoder passwordEncoder;
-    private final ObjectMapper objectMapper;
-    private final JwtService jwtService;
+
+    PlayerService(PlayerRepository playerRepository, PlayerProfileRepository playerProfileRepository, PasswordEncoder passwordEncoder) {
+        this.playerRepository = playerRepository;
+        this.playerProfileRepository = playerProfileRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     void registerPlayer(PlayerRegistrationRequest request) {
         checkPlayerExists(request);
 
         String passwordEncoded = passwordEncoder.encode(request.password());
-        Player player = savePlayer(request, passwordEncoded);
-        savePlayerProfile(player);
-    }
-
-    PlayerDto getPlayerById(UUID playerId) {
-        Player player = playerRepository.findById(playerId)
-                .orElseThrow(() -> new PlayerNotFoundException("Player not found"));
-
-        PlayerProfile profile = playerProfileRepository.findByPlayerId(playerId)
-                .orElseThrow(() -> new PlayerProfileNotFoundException("Player not found"));
-
-        PlayerDto playerDto = objectMapper.convertValue(player, PlayerDto.class);
-        PlayerProfileDto profileDto = objectMapper.convertValue(profile, PlayerProfileDto.class);
-
-        return new PlayerDto(
-                playerDto.id(),
-                playerDto.username(),
-                playerDto.email(),
-                playerDto.phone(),
-                playerDto.active(),
-                profile.getCreatedAt(),
-                profileDto
-        );
-
-    }
-
-    List<PlayerDto> getAllPlayers() {
-        List<Player> players = playerRepository.findAll();
-        log.info("Players: {}", players);
-        return players.stream()
-                .map(player -> objectMapper.convertValue(player, PlayerDto.class))
-                .toList();
+        Player playerSaved = savePlayer(request, passwordEncoded);
+        savePlayerProfile(playerSaved);
     }
 
     void updatePlayerStatus(UUID playerId, boolean isActive) {
         Player player = playerRepository.findById(playerId)
                 .orElseThrow(() -> new PlayerNotFoundException("Player not found"));
 
-        player.setActive(isActive);
-        playerRepository.save(player);
+        Player playerUpdate = Player.builder()
+                .active(isActive)
+                .phone(player.getPhone())
+                .email(player.getEmail())
+                .phone(player.getPhone())
+                .username(player.getUsername())
+                .password(passwordEncoder.encode(player.getPassword()))
+                .build();
+
+        playerRepository.save(playerUpdate);
     }
 
     private Player savePlayer(PlayerRegistrationRequest request, String passwordEncoded) {
@@ -88,22 +60,19 @@ class PlayerService {
     }
 
     private void checkPlayerExists(PlayerRegistrationRequest request) {
-        checkEmailAvailable(request);
-        checkUsernameAvailable(request);
-    }
-
-    private void checkUsernameAvailable(PlayerRegistrationRequest request) {
-        playerRepository.findByUsername(request.username())
-                .ifPresent(player -> {
-                    throw new PlayerAlreadyExistsException("Username already exists");
-                });
-    }
-
-    private void checkEmailAvailable(PlayerRegistrationRequest request) {
         if (playerRepository.findByEmail(request.email())
+                .stream()
+                .findAny()
                 .isPresent()) {
             throw new PlayerAlreadyExistsException("Email already exists");
         }
+
+        playerRepository.findByUsername(request.username())
+                .stream()
+                .findAny()
+                .ifPresent(player -> {
+                    throw new PlayerAlreadyExistsException("Username already exists");
+                });
     }
 
     private void savePlayerProfile(Player playerSaved) {
@@ -122,27 +91,15 @@ class PlayerService {
                 .stream()
                 .findAny()
                 .orElseThrow(() -> new PlayerNotFoundException("Player not found"));
-
         if (!passwordEncoder.matches(request.password(), player.getPassword())) {
-            throw new LoginInvalidCredentialsException("Invalid credentials");
+            throw new InvalidCredentialsException("Invalid credentials");
         }
 
-        String token = jwtService.generateToken(player.getUsername());
+        String token = generateToken(player);
         return new PlayerLoginDto(player.getId(), token);
     }
 
-    void deletePlayer(UUID playerId) {
-        playerRepository.deleteById(playerId);
-    }
-
-    void changePassword(UUID playerId, PasswordRequestDto request) {
-        Player player = playerRepository.findById(playerId).orElseThrow(() -> new PlayerNotFoundException("Player not found"));
-        if (!passwordEncoder.matches(request.oldPassword(), player.getPassword())) {
-            throw new InvalidPasswordException("Old password is incorrect");
-        }
-
-        String passwordEncoded = passwordEncoder.encode(request.newPassword());
-        player.setPassword(passwordEncoded);
-        playerRepository.save(player);
+    private String generateToken(Player player) {
+        return "";
     }
 }
--- src/main/java/pl/lotto/domain/player/dto/PlayerStatisticsDto.java
+++ src/main/java/pl/lotto/domain/player/dto/PlayerStatisticsDto.java
@@ -1,11 +1,8 @@
 package pl.lotto.domain.player.dto;
 
-import lombok.Builder;
-
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@Builder
 public record PlayerStatisticsDto(
         UUID playerId,
         int totalGamesPlayed,

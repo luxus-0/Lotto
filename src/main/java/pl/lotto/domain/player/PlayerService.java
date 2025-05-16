4785503a2dafdcc1a298575b0df5d6e85f8f96f4
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-16 23:23:06 +0200

    feat(player): implement player module with jwt, crud, login, document

--- src/main/java/pl/lotto/domain/player/PlayerService.java
+++ src/main/java/pl/lotto/domain/player/PlayerService.java
@@ -1,47 +1,75 @@
 package pl.lotto.domain.player;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.lotto.domain.player.dto.*;
+import pl.lotto.infrastructure.security.JwtService;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+/*
+*Dokończyć metodę: String generateToken() z JWT
+* Dokończyć metodę: PlayerStatisticsDto getPlayerStatistics(UUID playerId);
+* */
 @Service
 @Log4j2
+@AllArgsConstructor
 class PlayerService {
     private final PlayerRepository playerRepository;
     private final PlayerProfileRepository playerProfileRepository;
     private final PasswordEncoder passwordEncoder;
-
-    PlayerService(PlayerRepository playerRepository, PlayerProfileRepository playerProfileRepository, PasswordEncoder passwordEncoder) {
-        this.playerRepository = playerRepository;
-        this.playerProfileRepository = playerProfileRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final ObjectMapper objectMapper;
+    private final JwtService jwtService;
 
     void registerPlayer(PlayerRegistrationRequest request) {
         checkPlayerExists(request);
 
         String passwordEncoded = passwordEncoder.encode(request.password());
-        Player playerSaved = savePlayer(request, passwordEncoded);
-        savePlayerProfile(playerSaved);
+        Player player = savePlayer(request, passwordEncoded);
+        savePlayerProfile(player);
+    }
+
+    PlayerDto getPlayerById(UUID playerId) {
+        Player player = playerRepository.findById(playerId)
+                .orElseThrow(() -> new PlayerNotFoundException("Player not found"));
+
+        PlayerProfile profile = playerProfileRepository.findByPlayerId(playerId)
+                .orElseThrow(() -> new PlayerProfileNotFoundException("Player not found"));
+
+        PlayerDto playerDto = objectMapper.convertValue(player, PlayerDto.class);
+        PlayerProfileDto profileDto = objectMapper.convertValue(profile, PlayerProfileDto.class);
+
+        return new PlayerDto(
+                playerDto.id(),
+                playerDto.username(),
+                playerDto.email(),
+                playerDto.phone(),
+                playerDto.active(),
+                profile.getCreatedAt(),
+                profileDto
+        );
+
+    }
+
+    List<PlayerDto> getAllPlayers() {
+        List<Player> players = playerRepository.findAll();
+        log.info("Players: {}", players);
+        return players.stream()
+                .map(player -> objectMapper.convertValue(player, PlayerDto.class))
+                .toList();
     }
 
     void updatePlayerStatus(UUID playerId, boolean isActive) {
         Player player = playerRepository.findById(playerId)
                 .orElseThrow(() -> new PlayerNotFoundException("Player not found"));
 
-        Player playerUpdate = Player.builder()
-                .active(isActive)
-                .phone(player.getPhone())
-                .email(player.getEmail())
-                .phone(player.getPhone())
-                .username(player.getUsername())
-                .password(passwordEncoder.encode(player.getPassword()))
-                .build();
-
-        playerRepository.save(playerUpdate);
+        player.setActive(isActive);
+        playerRepository.save(player);
     }
 
     private Player savePlayer(PlayerRegistrationRequest request, String passwordEncoded) {
@@ -60,19 +88,22 @@ class PlayerService {
     }
 
     private void checkPlayerExists(PlayerRegistrationRequest request) {
-        if (playerRepository.findByEmail(request.email())
-                .stream()
-                .findAny()
-                .isPresent()) {
-            throw new PlayerAlreadyExistsException("Email already exists");
-        }
+        checkEmailAvailable(request);
+        checkUsernameAvailable(request);
+    }
 
+    private void checkUsernameAvailable(PlayerRegistrationRequest request) {
         playerRepository.findByUsername(request.username())
-                .stream()
-                .findAny()
                 .ifPresent(player -> {
                     throw new PlayerAlreadyExistsException("Username already exists");
                 });
+    }
+
+    private void checkEmailAvailable(PlayerRegistrationRequest request) {
+        if (playerRepository.findByEmail(request.email())
+                .isPresent()) {
+            throw new PlayerAlreadyExistsException("Email already exists");
+        }
     }
 
     private void savePlayerProfile(Player playerSaved) {
@@ -91,15 +122,27 @@ class PlayerService {
                 .stream()
                 .findAny()
                 .orElseThrow(() -> new PlayerNotFoundException("Player not found"));
+
         if (!passwordEncoder.matches(request.password(), player.getPassword())) {
-            throw new InvalidCredentialsException("Invalid credentials");
+            throw new LoginInvalidCredentialsException("Invalid credentials");
         }
 
-        String token = generateToken(player);
+        String token = jwtService.generateToken(player.getUsername());
         return new PlayerLoginDto(player.getId(), token);
     }
 
-    private String generateToken(Player player) {
-        return "";
+    void deletePlayer(UUID playerId) {
+        playerRepository.deleteById(playerId);
+    }
+
+    void changePassword(UUID playerId, PasswordRequestDto request) {
+        Player player = playerRepository.findById(playerId).orElseThrow(() -> new PlayerNotFoundException("Player not found"));
+        if (!passwordEncoder.matches(request.oldPassword(), player.getPassword())) {
+            throw new InvalidPasswordException("Old password is incorrect");
+        }
+
+        String passwordEncoded = passwordEncoder.encode(request.newPassword());
+        player.setPassword(passwordEncoded);
+        playerRepository.save(player);
     }
 }

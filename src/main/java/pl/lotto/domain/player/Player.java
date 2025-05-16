6d6211e786a54f0fbffac9bda28d6c2d3d0ee0d7
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-16 23:25:38 +0200

    Revert "feat(player):"
    This reverts commit 5d358ed00861b2c467b0086edcec1081dcd255b6.

--- src/main/java/pl/lotto/domain/player/Player.java
+++ src/main/java/pl/lotto/domain/player/Player.java
@@ -29,4 +29,12 @@ public class Player {
     private String email;
     private String password;
     private boolean active;
+
+    public Player(String username, String phone, String email, boolean active) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.phone = phone;
+        this.email = email;
+        this.active = active;
+    }
 }

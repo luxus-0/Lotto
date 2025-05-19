ce546a97c1b1de20be472787bec5159661dc99a0
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-19 21:29:25 +0200

    Kafka integration test - fail(problem)

--- src/test/java/integration/kafka/KafkaIntegrationTest.java
+++ src/test/java/integration/kafka/KafkaIntegrationTest.java
@@ -56,13 +56,6 @@ public class KafkaIntegrationTest {
     @DynamicPropertySource
     static void kafkaProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-        registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
-        registry.add("spring.kafka.consumer.group-id", () -> "test-group");
-        registry.add("spring.kafka.producer.key-serializer", () -> "org.apache.kafka.common.serialization.StringSerializer");
-        registry.add("spring.kafka.producer.value-serializer", () -> "org.springframework.kafka.support.serializer.JsonSerializer");
-        registry.add("spring.kafka.consumer.key-deserializer", () -> "org.apache.kafka.common.serialization.StringDeserializer");
-        registry.add("spring.kafka.consumer.value-deserializer", () -> "org.springframework.kafka.support.serializer.JsonDeserializer");
-        registry.add("spring.kafka.consumer.properties.spring.json.trusted.packages", () -> "*");
     }
 
     @BeforeEach

1373e317dae6edcd4755419fea9eec7512a36086
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-19 21:23:44 +0200

    Refactor code

--- src/main/java/pl/lotto/domain/draw/LotteryDrawScheduler.java
+++ src/main/java/pl/lotto/domain/draw/LotteryDrawScheduler.java
@@ -14,7 +14,7 @@ import java.util.random.RandomGenerator;
 @Component
 @AllArgsConstructor
 @Log4j2
-public class LotteryDrawScheduler {
+class LotteryDrawScheduler {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final LotteryDrawConfigurationProperties properties;

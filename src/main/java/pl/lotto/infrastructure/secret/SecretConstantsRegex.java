ecaca06585dc678652932a9eaeb1ae35559139a8
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-22 22:06:59 +0200

    remove unused class

--- src/main/java/pl/lotto/infrastructure/secret/SecretConstantsRegex.java
+++
@@ -1,9 +0,0 @@
-package pl.lotto.infrastructure.secret;
-
-public class SecretConstantsRegex {
-    public static final String PASSWORD_PATTERN =
-            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&*!])[A-Za-z\\d@#$%^&*!]{8,}$";
-
-    public static final String PASSWORD_MESSAGE =
-            "Password must contain at least 8 characters, including uppercase, lowercase, digit, and special character (@#$%^&*!).";
-}

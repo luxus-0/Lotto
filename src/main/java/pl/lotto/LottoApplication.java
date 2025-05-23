73bbd472dfea621ffdad0a71c37e2efa8bdb9eb5
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-05-23 13:46:40 +0200

    Refactor

--- src/main/java/pl/lotto/LottoApplication.java
+++ src/main/java/pl/lotto/LottoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import pl.lotto.domain.drawdatetime.DrawDateTimeConfigurationProperties;
-import pl.lotto.domain.randomnumbers.RandomNumbersConfigurationProperties;
+import pl.lotto.domain.randomnumbers.RandomNumbersValidatorConfigurationProperties;
 import pl.lotto.infrastructure.token.TokenConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({TokenConfigurationProperties.class, RandomNumbersConfigurationProperties.class, DrawDateTimeConfigurationProperties.class})
+@EnableConfigurationProperties({TokenConfigurationProperties.class, RandomNumbersValidatorConfigurationProperties.class, DrawDateTimeConfigurationProperties.class})
 public class LottoApplication {
 
     public static void main(String[] args) {
--- src/main/java/pl/lotto/domain/player/Player.java
+++ src/main/java/pl/lotto/domain/player/Player.java
@@ -1,31 +1,21 @@
 package pl.lotto.domain.player;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document(collection = "players")
 @Builder
-@Getter
-public class Player {
-    @Id
-    private UUID id;
-    @NotBlank(message = "Username cannot be blank")
-    private String username;
-    @Pattern(regexp = "^\\+?[1-9][0-9]{7,14}$", message = "Invalid phone number")
-    private String phone;
-    @Email(message = "Invalid email")
-    private String email;
-    private String password;
-    private boolean active;
+public record Player(
+        @Id
+        UUID id,
+        String name,
+        String surname,
+        String phone,
+        String address,
+        LocalDateTime createdAt
+) {
 }

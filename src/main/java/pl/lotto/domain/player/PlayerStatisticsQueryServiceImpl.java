177cee25f5bfa65568af755ef9063c73c91a570c
luxus-0 <luxus0@gmail.com>
2025-05-27 21:08:36 +0200

    Empty class

---
+++ src/main/java/pl/lotto/domain/player/PlayerStatisticsQueryServiceImpl.java
@@ -0,0 +1,24 @@
+package pl.lotto.domain.player;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.lotto.domain.drawdatetime.DrawDateTimeQueryServiceImpl;
+import pl.lotto.domain.player.dto.PlayerRequest;
+import pl.lotto.domain.player.dto.PlayerStatistics;
+import pl.lotto.domain.randomnumbers.RandomNumbersGeneratorQueryServiceImpl;
+import pl.lotto.domain.ticket.TicketQueryServiceImpl;
+
+@Service
+@AllArgsConstructor
+public class PlayerStatisticsQueryServiceImpl implements PlayerStatisticsQueryService {
+    private final RandomNumbersGeneratorQueryServiceImpl randomNumbersQueryServiceImpl;
+    private final DrawDateTimeQueryServiceImpl drawDateTimeQueryServiceImpl;
+    private final TicketQueryServiceImpl ticketQueryService;
+
+    @Override
+    public PlayerStatistics generateStats(PlayerRequest playerRequest) {
+
+        return PlayerStatistics.builder()
+                .build();
+    }
+}

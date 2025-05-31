cb1f84fcc5a640fe92d1337f484ce2320f76ba7c
luxus-0 <luxus0@gmail.com>
2025-05-31 18:33:58 +0200

    Refactor

--- src/main/java/pl/lotto/domain/ticket/TicketNumbersValidatorConfigurationProperties.java
+++ src/main/java/pl/lotto/domain/ticket/TicketNumbersValidatorConfigurationProperties.java
@@ -1,5 +1,7 @@
 package pl.lotto.domain.ticket;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -8,6 +10,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Builder
 @Getter
 public class TicketNumbersValidatorConfigurationProperties {
+    @Min(1)
     private Integer min;
+    @Max(99)
     private Integer max;
 }

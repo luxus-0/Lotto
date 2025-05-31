9404b389927cc173d9bbed4eb82d0c570157942e
luxus-0 <luxus0@gmail.com>
2025-05-31 18:29:14 +0200

    Refactor

--- src/main/java/pl/lotto/domain/randomnumbers/RandomNumbersValidatorConfigurationProperties.java
+++ src/main/java/pl/lotto/domain/randomnumbers/RandomNumbersValidatorConfigurationProperties.java
@@ -2,11 +2,9 @@ package pl.lotto.domain.randomnumbers;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import lombok.Builder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "random.numbers")
-@Builder
 public record RandomNumbersValidatorConfigurationProperties(
         @Min(1) int min,
         @Max(99) int max,

e1332f8f4edd106083ff9f0c2957f2cd66fff398
luxus-0 <luxus0@gmail.com>
2025-06-08 17:12:19 +0200

    feature - add SendgridConfiguration

---
+++ src/main/java/pl/lotto/infrastructure/configuration/SendgridConfiguration.java
@@ -0,0 +1,14 @@
+package pl.lotto.infrastructure.configuration;
+
+import com.sendgrid.SendGrid;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.lotto.domain.notification.EmailConfigurationProperties;
+
+@Configuration
+public class SendgridConfiguration {
+    @Bean
+    public SendGrid sendGrid(EmailConfigurationProperties properties) {
+        return new SendGrid(properties.apiKey());
+    }
+}
